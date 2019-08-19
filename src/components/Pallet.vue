7be3480f2dd8d0cd73e6b51d8232fe6c8fccdb8e
Theo <theoproulx@gmail.com>
2019-08-18 20:05:59 -0400

    streamlined processing, added sorting

--- src/components/Pallet.vue
+++ src/components/Pallet.vue
@@ -1,13 +1,37 @@
 <template>
   <v-container fluid>
+    <v-layout justify-center mb-4>
+      <v-btn color="red" class="white--text" @click="changeSort('types')">TYPES</v-btn>
+      <v-btn color="green" class="white--text" @click="changeSort('gens')">GENS</v-btn>
+      <v-btn color="blue" class="white--text" @click="changeSort('colors')">COLORS</v-btn>
+      <v-btn color="yellow" class="white--text" @click="changeSort('starter_lines')">STARTERS</v-btn>
+    </v-layout>
     <v-layout justify-center>
       <v-flex xs6>
-        <v-card>
+        <v-card v-if="sortBy === 'rating'">
           <v-card-text
             v-for="mon in pokemon"
             :key="mon.data.name"
             class="title text-xs-left"
           >{{ mon.data.name }}: {{ numToText(mon.data.rating) }}</v-card-text>
+        </v-card>
+        <v-card v-else-if="sortBy === 'starter_lines'">
+          <v-card-text
+            v-for="x in sortedArray"
+            :key="sortedArray.indexOf(x)"
+            class="title text-xs-left"
+          >
+            <span v-for="mon in pokemon" :key="mon.id">
+              <span v-if="mon.id === x.key">{{ mon.data.name }}: {{ x.avg }}</span>
+            </span>
+          </v-card-text>
+        </v-card>
+        <v-card v-else>
+          <v-card-text
+            v-for="x in sortedArray"
+            :key="sortedArray.indexOf(x)"
+            class="title text-xs-left"
+          >{{ x.key }}: {{ x.avg }}</v-card-text>
         </v-card>
       </v-flex>
     </v-layout>
@@ -15,27 +39,82 @@
 </template>
 
 <script>
+
+function takeAvg (array) {
+  array.forEach(x => {
+    if (x.count > 0) {
+      x.avg = x.score / x.count
+    } else {
+      x.avg = 0
+    }
+  })
+  return array
+}
+
+function avgSort (a, b) {
+  if (a.avg > b.avg) {
+    return -1
+  } else if (a.avg < b.avg) {
+    return 1
+  }
+  return 0
+}
+
+function valueCheck (array, value, v, rating) {
+  let dict
+  array.forEach(d => {
+    let val
+    if (value === 'line') {
+      val = d.line
+    } else if (value === 'gen') {
+      val = d.gen
+    }
+    if (val === v) {
+      dict = d
+    }
+  })
+  if (dict) {
+    dict.count++
+    dict.score += rating
+  } else {
+    array.push({
+      key: v,
+      count: 1,
+      score: rating,
+      avg: 0
+    })
+  }
+  return array
+}
+
 export default {
   data () {
     return {
       mode: this.$route.params.mode,
       pokemon: this.$route.params.pokemon,
+      sortBy: 'rating',
       ts: ['normal', 'fire', 'fighting', 'water', 'flying', 'grass', 'poison', 'electric', 'ground', 'psychic', 'rock', 'ice', 'bug', 'dragon', 'ghost', 'dark', 'steel', 'fairy'],
       gs: ['1', '2', '3', '4', '5', '6', '7'],
       cs: ['red', 'blue', 'yellow', 'green', 'black', 'brown', 'purple', 'gray', 'white', 'pink'],
-      types: {},
-      gens: {},
-      colors: {},
-      two_stages: {},
-      three_stages: {},
-      pseudo_lines: {},
-      starter_lines: {},
-      gens_by_starters: {},
-      regional_birds: {},
-      regional_rodents: {},
-      regional_bugs: {},
-      regional_sets: {},
-      regional_sets_with_starters: {}
+      types: [],
+      gens: [],
+      colors: [],
+      two_stages: [],
+      three_stages: [],
+      pseudo_lines: [],
+      starter_lines: [],
+      gens_by_starters: [],
+      regional_birds: [],
+      regional_rodents: [],
+      regional_bugs: [],
+      regional_sets: [],
+      regional_sets_with_starters: []
+    }
+  },
+  computed: {
+    sortedArray () {
+      /* eslint-disable no-eval */
+      return eval('this.' + this.sortBy)
     }
   },
   mounted () {
@@ -44,35 +123,43 @@ export default {
   methods: {
     process () {
       // initialize types
-      this.ts.foreach(t => {
-        this.types[t] = {
+      this.ts.forEach(t => {
+        this.types.push({
+          key: t,
           count: 0,
           score: 0,
           avg: 0
-        }
+        })
       })
       // initialize gens
-      this.gs.foreach(g => {
-        this.gens[g] = {
+      this.gs.forEach(g => {
+        this.gens.push({
+          key: g,
           count: 0,
           score: 0,
           avg: 0
-        }
+        })
       })
       // initialize colors
-      this.cs.foreach(c => {
-        this.colors[c] = {
+      this.cs.forEach(c => {
+        this.colors.push({
+          key: c,
           count: 0,
           score: 0,
           avg: 0
-        }
+        })
       })
       // for each pokemon
-      this.pokemon.foreach(mon => {
+      this.pokemon.forEach(mon => {
+        mon = mon.data
         // none of this is relevant to mega evolutions
         if (!mon.mega) {
           // extract relevant attributes from current pokemon
-          let type = mon.type
+          let type1 = mon.types[0]
+          let type2
+          if (mon.types.length > 1) {
+            type2 = mon.types[1]
+          }
           let gen = mon.gen
           let color = mon.color
           let rating = mon.rating
@@ -84,225 +171,96 @@ export default {
           let regionalRodent = mon.regional_rodent
           let regionalBug = mon.regional_bug
           // add in data for current pokemon's type, gen, color
-          this.types[type].count++
-          this.types[type].score += rating
-          this.gens[gen].count++
-          this.gens[gen].score += rating
-          this.colors[color].count++
-          this.colors[color].score += rating
+          let typeDict1
+          this.types.forEach(t => {
+            if (t.key === type1) {
+              typeDict1 = t
+            }
+          })
+          let typeDict2
+          if (type2) {
+            this.types.forEach(t => {
+              if (t.key === type2) {
+                typeDict2 = t
+              }
+            })
+          }
+          let genDict
+          this.gens.forEach(g => {
+            if (g.key === gen) {
+              genDict = g
+            }
+          })
+          let colorDict
+          this.colors.forEach(c => {
+            if (c.key === color) {
+              colorDict = c
+            }
+          })
+          typeDict1.count++
+          typeDict1.score += rating
+          if (typeDict2) {
+            typeDict2.count++
+            typeDict2.score += rating
+          }
+          genDict.count++
+          genDict.score += rating
+          colorDict.count++
+          colorDict.score += rating
           // if pokemon is in a 2-stage line
           if (stages === 2) {
-            if (line in this.two_stages) {
-              this.two_stages[line].count++
-              this.two_stages[line].score += rating
-            } else {
-              this.two_stages[line] = {
-                count: 0,
-                score: 0,
-                avg: 0
-              }
-            }
+            this.two_stages = valueCheck(this.two_stages, 'line', line, rating)
           }
           // if pokemon is in a 3-stage line
           if (stages === 3) {
-            if (line in this.three_stages) {
-              this.three_stages[line].count++
-              this.three_stages[line].score += rating
-            } else {
-              this.three_stages[line] = {
-                count: 0,
-                score: 0,
-                avg: 0
-              }
-            }
+            this.three_stages = valueCheck(this.three_stages, 'line', line, rating)
           }
           // if pokemon is in a pseudo-legendary line
           if (pseudo) {
-            if (line in this.pseudo_lines) {
-              this.pseudo_lines[line].count++
-              this.pseudo_lines[line].score += rating
-            } else {
-              this.pseudo_lines[line] = {
-                count: 0,
-                score: 0,
-                avg: 0
-              }
-            }
+            this.pseudo_lines = valueCheck(this.pseudo_lines, 'line', line, rating)
           }
           // if pokemon is in a starter line
           if (starter) {
-            if (line in this.starter_lines) {
-              this.starter_lines[line].count++
-              this.starter_lines[line].score += rating
-            } else {
-              this.starter_lines[line] = {
-                count: 0,
-                score: 0,
-                avg: 0
-              }
-            }
-            if (gen in this.gens_by_starters) {
-              this.gens_by_starters[gen].count++
-              this.gens_by_starters[gen].score += rating
-            } else {
-              this.gens_by_starters[gen] = {
-                count: 0,
-                score: 0,
-                avg: 0
-              }
-            }
-            if (gen in this.regional_set_with_starters) {
-              this.regional_set_with_starters[gen].count++
-              this.regional_set_with_starters[gen].score += rating
-            } else {
-              this.regional_set_with_starters[gen] = {
-                count: 0,
-                score: 0,
-                avg: 0
-              }
-            }
+            console.log(this.starter_lines)
+            this.starter_lines = valueCheck(this.starter_lines, 'line', line, rating)
+            console.log(this.starter_lines)
+            this.gens_by_starters = valueCheck(this.gens_by_starters, 'gen', gen, rating)
+            this.regional_sets_with_starters = valueCheck(this.regional_sets_with_starters, 'gen', gen, rating)
           }
           // if pokemon is in a regional bird line
           if (regionalBird) {
-            if (line in this.regional_birds) {
-              this.regional_birds[line].count++
-              this.regional_birds[line].score += rating
-            } else {
-              this.regional_birds[line] = {
-                count: 0,
-                score: 0,
-                avg: 0
-              }
-            }
-            if (gen in this.regional_sets) {
-              this.regional_sets[gen].count++
-              this.regional_sets[gen].score += rating
-            } else {
-              this.regional_sets[gen] = {
-                count: 0,
-                score: 0,
-                avg: 0
-              }
-            }
-            if (gen in this.regional_set_with_starters) {
-              this.regional_set_with_starters[gen].count++
-              this.regional_set_with_starters[gen].score += rating
-            } else {
-              this.regional_set_with_starters[gen] = {
-                count: 0,
-                score: 0,
-                avg: 0
-              }
-            }
+            this.regional_birds = valueCheck(this.regional_birds, 'line', line, rating)
+            this.regional_sets = valueCheck(this.regional_sets, 'gen', gen, rating)
+            this.regional_sets_with_starters = valueCheck(this.regional_sets_with_starters, 'gen', gen, rating)
           }
           // if pokemon is in a regional rodent line
           if (regionalRodent) {
-            if (line in this.regional_rodents) {
-              this.regional_rodents[line].count++
-              this.regional_rodents[line].score += rating
-            } else {
-              this.regional_rodents[line] = {
-                count: 0,
-                score: 0,
-                avg: 0
-              }
-            }
-            if (gen in this.regional_sets) {
-              this.regional_sets[gen].count++
-              this.regional_sets[gen].score += rating
-            } else {
-              this.regional_sets[gen] = {
-                count: 0,
-                score: 0,
-                avg: 0
-              }
-            }
-            if (gen in this.regional_set_with_starters) {
-              this.regional_set_with_starters[gen].count++
-              this.regional_set_with_starters[gen].score += rating
-            } else {
-              this.regional_set_with_starters[gen] = {
-                count: 0,
-                score: 0,
-                avg: 0
-              }
-            }
+            this.regional_rodents = valueCheck(this.regional_rodents, 'line', line, rating)
+            this.regional_sets = valueCheck(this.regional_sets, 'gen', gen, rating)
+            this.regional_sets_with_starters = valueCheck(this.regional_sets_with_starters, 'gen', gen, rating)
           }
           // if pokemon is in a regional bug line
           if (regionalBug) {
-            if (line in this.regional_bugs) {
-              this.regional_bugs[line].count++
-              this.regional_bugs[line].score += rating
-            } else {
-              this.regional_bugs[line] = {
-                count: 0,
-                score: 0,
-                avg: 0
-              }
-            }
-            if (gen in this.regional_sets) {
-              this.regional_sets[gen].count++
-              this.regional_sets[gen].score += rating
-            } else {
-              this.regional_sets[gen] = {
-                count: 0,
-                score: 0,
-                avg: 0
-              }
-            }
-            if (gen in this.regional_set_with_starters) {
-              this.regional_set_with_starters[gen].count++
-              this.regional_set_with_starters[gen].score += rating
-            } else {
-              this.regional_set_with_starters[gen] = {
-                count: 0,
-                score: 0,
-                avg: 0
-              }
-            }
+            this.regional_bugs = valueCheck(this.regional_bugs, 'line', line, rating)
+            this.regional_sets = valueCheck(this.regional_sets, 'gen', gen, rating)
+            this.regional_sets_with_starters = valueCheck(this.regional_sets_with_starters, 'gen', gen, rating)
           }
-          // set averages
-          this.types.foreach(x => {
-            x.avg = x.score / x.count
-          })
-          this.gens.foreach(x => {
-            x.avg = x.score / x.count
-          })
-          this.colors.foreach(x => {
-            x.avg = x.score / x.count
-          })
-          this.two_stages.foreach(x => {
-            x.avg = x.score / x.count
-          })
-          this.three_stages.foreach(x => {
-            x.avg = x.score / x.count
-          })
-          this.pseudo_lines.foreach(x => {
-            x.avg = x.score / x.count
-          })
-          this.starter_lines.foreach(x => {
-            x.avg = x.score / x.count
-          })
-          this.gens_by_starters.foreach(x => {
-            x.avg = x.score / x.count
-          })
-          this.regional_birds.foreach(x => {
-            x.avg = x.score / x.count
-          })
-          this.regional_rodents.foreach(x => {
-            x.avg = x.score / x.count
-          })
-          this.regional_bugs.foreach(x => {
-            x.avg = x.score / x.count
-          })
-          this.regional_sets.foreach(x => {
-            x.avg = x.score / x.count
-          })
-          this.regional_sets_with_starters.foreach(x => {
-            x.avg = x.score / x.count
-          })
         }
       })
+      // set and sort by averages
+      this.types = takeAvg(this.types).sort(avgSort)
+      this.gens = takeAvg(this.gens).sort(avgSort)
+      this.colors = takeAvg(this.colors).sort(avgSort)
+      this.two_stages = takeAvg(this.two_stages).sort(avgSort)
+      this.three_stages = takeAvg(this.three_stages).sort(avgSort)
+      this.pseudo_lines = takeAvg(this.pseudo_lines).sort(avgSort)
+      this.starter_lines = takeAvg(this.starter_lines).sort(avgSort)
+      this.gens_by_starters = takeAvg(this.gens_by_starters).sort(avgSort)
+      this.regional_birds = takeAvg(this.regional_birds).sort(avgSort)
+      this.regional_rodents = takeAvg(this.regional_rodents).sort(avgSort)
+      this.regional_bugs = takeAvg(this.regional_bugs).sort(avgSort)
+      this.regional_sets = takeAvg(this.regional_sets).sort(avgSort)
+      this.regional_sets_with_starters = takeAvg(this.regional_sets_with_starters).sort(avgSort)
     },
     numToText (n) {
       if (n === 1) {
@@ -346,6 +304,9 @@ export default {
       } else if (n === 10) {
         return '10'
       }
+    },
+    changeSort (s) {
+      this.sortBy = s
     }
   },
   beforeRouteLeave (to, from, next) {
