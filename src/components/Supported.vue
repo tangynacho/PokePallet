5fa5c1fb5c351bb655cfe51db77d1c4765af77b9
Theo <theoproulx@gmail.com>
2019-11-05 20:06:20 -0500

    finished supported page

--- src/components/Supported.vue
+++ src/components/Supported.vue
@@ -2,9 +2,28 @@
   <v-container fluid>
     <v-layout justify-center>
       <v-flex xs12 xl6>
-        <p :class="bigtext">Currently Supported Stats</p>
+        <p :class="bigtext">Currently, this app can be used to rank your favorite:</p>
         <ul :class="`${medtext} text-xs-left`">
-          <li>stat 1</li>
+          <li>Pokemon</li>
+          <li>Type of Pokemon</li>
+          <li>Generation of Pokemon</li>
+          <li>Color of Pokemon</li>
+          <li>2-Stage Line</li>
+          <li>3-Stage Line</li>
+          <li>Pseudo Legendary Line</li>
+          <li>Starter Line</li>
+          <li>Generation by Starter Line</li>
+          <li>Regional Bird Line</li>
+          <li>Regional Rodent Line</li>
+          <li>Regional Bug Line</li>
+          <li>Regional Set (Starters, Birds, Rodents and Bugs)</li>
+          <li>X Types, where X is any existing Pokemon Type</li>
+          <li>Pokemon from Generation X, where X is an existing Generation of Pokemon</li>
+          <li>X Colored Pokemon, where X is an existing official Pokedex Color</li>
+          <li>Single Form Pokemon</li>
+          <li>First Form Starter Pokemon</li>
+          <li>Middle Form Starter Pokemon</li>
+          <li>Final Form Starter Pokemon</li>
         </ul>
       </v-flex>
     </v-layout>
